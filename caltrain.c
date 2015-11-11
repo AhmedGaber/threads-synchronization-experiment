8ac6b4cfa2dfa53e4c2b61c6283dfec071e8a1e3
Ahmed Gaber <ahmedgaber20@gmail.com>
2015-11-11 22:52:28 +0200

    Implement the station struct

--- caltrain.c
+++ caltrain.c
@@ -6,7 +6,12 @@
 void
 station_init(struct station *station)
 {
-	// FILL ME IN
+  station->out_passengers = 0;
+  station->in_passengers = 0;
+  pthread_mutex_init(&(station->lock), NULL);
+  pthread_cond_init(&(station->train_arrived_cond), NULL);
+  pthread_cond_init(&(station->passengers_seated_cond), NULL);
+  pthread_cond_init(&(station->train_is_full_cond), NULL);
 }
 
 void
--- caltrain.h
+++ caltrain.h
@@ -4,9 +4,9 @@ struct station {
   int out_passengers; // in station waiting passengers
   int in_passengers; // in train passengers 
   pthread_mutex_t lock;
-  pthread_cond_t cond_train_arrive;
-  pthread_cond_t cond_passengers_seated;
-  pthread_cond_t cond_train_is_full;
+  pthread_cond_t train_arrived_cond;
+  pthread_cond_t passengers_seated_cond;
+  pthread_cond_t train_is_full_cond;
 };
 
 void station_init(struct station *station);
