08215a69546cdb1c960c9a6def70af90fca1bb51
Ahmed Gaber <ahmedgaber20@gmail.com>
2015-11-12 23:17:59 +0200

    Implement station_load_train

--- caltrain.c
+++ caltrain.c
@@ -17,7 +17,20 @@ station_init(struct station *station)
 void
 station_load_train(struct station *station, int count)
 {
-	// FILL ME IN
+  // Enter critical region
+  pthread_mutex_lock(&(station->lock));
+
+  while ((station->out_passengers > 0) && (count > 0)){
+    pthread_cond_signal(&(station->train_arrived_cond));
+  	count--;
+  	pthread_cond_wait(&(station->passengers_seated_cond), &(station->lock));
+  }
+
+  if (station->in_passengers > 0)
+  	pthread_cond_wait(&(station->train_is_full_cond), &(station->lock));
+
+  // Leave critical region
+  pthread_mutex_unlock(&(station->lock));
 }
 
 void
@@ -31,3 +44,9 @@ station_on_board(struct station *station)
 {
 	// FILL ME IN
 }
+
+
+pthread_cond_wait(&cond_availData, &pc_mutex);
+pthread_cond_signal(&cond_availSpace);
+pthread_mutex_lock(&pc_mutex);
+pthread_mutex_unlock(&pc_mutex);
